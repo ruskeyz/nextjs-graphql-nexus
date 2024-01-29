31aa5740a7b13bd29454e3b8abaebded8e253a50
Ilya Playrcart <ilya@playrcart.com>
2024-01-29 21:26:01 +0000

    getAllTasks schema and ui

--- src/graphql-server/schema.ts
+++ src/graphql-server/schema.ts
@@ -12,48 +12,56 @@ import path from "path";
 import { Task } from "nexus-prisma";
 
 export const DateTime = asNexusMethod(DateTimeResolver, "date");
+export const Query = objectType({
+  name: "Query",
+  definition(t) {
+    t.field("getTask", {
+      type: "Task",
+      args: {
+        id: nonNull(intArg()),
+      },
+      resolve: (_parent, args, ctx: Context) => {
+        return ctx.prisma.task.findUnique({
+          where: { id: args.id },
+        });
+      },
+    });
+    t.list.nonNull.field("getAllTasks", {
+      type: "Task",
+      resolve: (_parent, __, ctx: Context) => {
+        return ctx.prisma.task.findMany();
+      },
+    });
+  },
+});
+export const Mutation = objectType({
+  name: "Mutation",
+  definition(t) {
+    t.nonNull.field("createTask", {
+      type: "Task",
+      args: {
+        title: nonNull(stringArg()),
+        description: stringArg(),
+        status: nonNull(stringArg()),
+      },
+      resolve: (_, args, ctx: Context) => {
+        return ctx.prisma.task.create({
+          data: {
+            title: args.title,
+            description: args.description,
+            status: args.status,
+          },
+        });
+      },
+    });
+  },
+});
 
 export const schema = makeSchema({
   types: [
     DateTime,
-    objectType({
-      name: "Query",
-      definition(t) {
-        t.field("getTask", {
-          type: "Task",
-          args: {
-            id: nonNull(intArg()),
-          },
-          resolve: (_parent, args, ctx: Context) => {
-            return ctx.prisma.task.findUnique({
-              where: { id: args.id },
-            });
-          },
-        });
-      },
-    }),
-    objectType({
-      name: "Mutation",
-      definition(t) {
-        t.nonNull.field("createTask", {
-          type: "Task",
-          args: {
-            title: nonNull(stringArg()),
-            description: stringArg(),
-            status: nonNull(stringArg()),
-          },
-          resolve: (_, args, ctx: Context) => {
-            return ctx.prisma.task.create({
-              data: {
-                title: args.title,
-                description: args.description,
-                status: args.status,
-              },
-            });
-          },
-        });
-      },
-    }),
+    Query,
+    Mutation,
     objectType({
       name: Task.$name,
       description: Task.$description,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { graphql } from "../gql";
+import { useQuery } from "@apollo/client";
+
+const GET_ALL_TASKS = graphql(`
+  query GetAllTasks {
+    getAllTasks {
+      title
+      status
+      id
+      description
+    }
+  }
+`);
 
 export default function Home() {
+  const { data, loading, error } = useQuery(GET_ALL_TASKS);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+  if (!data) {
+    return null;
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,20 +40,45 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to the Task Manager</h1>
 
-        <p className={styles.description}>Please read the README.md</p>
-
-        <p className={styles.description}>
-          GraphQL API located at
-          <a href="/api/graphql" target="_blank">
-            <code className={styles.code}>pages/api/graphql.js</code>
-          </a>
-        </p>
-
-        <p className={styles.description}>
-          <a className={styles.link} href="/task/">
-            Task Manager, solution
-          </a>
-        </p>
+        <h2>Create a task</h2>
+        <div className={styles.createTask}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="email" className={styles.label}>
+              Title
+            </label>
+            <input
+              type="title"
+              name="title"
+              id="title"
+              className={styles.input}
+              placeholder="Task Title"
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="email" className={styles.label}>
+              Description
+            </label>
+            <input
+              type="title"
+              name="title"
+              id="title"
+              className={styles.input}
+              placeholder="Task Description"
+            />
+          </div>
+          <button type="submit">Create Task</button>
+        </div>
+        <h2>All tasks</h2>
+        {data.getAllTasks.map((task) => {
+          return (
+            <div key={task.id} className={styles.card}>
+              <p>Title: {task.title}</p>
+              <p>Description: {task.description}</p>
+              <p>Status {task.status}</p>
+              <button>x</button>
+            </div>
+          );
+        })}
       </main>
     </div>
   );
