80558e121fe4c7c8bc6c6100efc720487b8de00a
Ilya Playrcart <ilya@playrcart.com>
2024-01-30 22:06:30 +0000

    createSubTask

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { graphql } from "../gql";
 import { useMutation, useQuery } from "@apollo/client";
 import { useState } from "react";
-
-const GET_ALL_TASKS = graphql(`
-  query GetAllTasks {
-    getAllTasks {
-      title
-      status
-      id
-      description
-    }
-  }
-`);
-
-const CREATE_TASK = graphql(`
-  mutation CreateTask($title: String!, $status: String!, $description: String) {
-    createTask(title: $title, status: $status, description: $description) {
-      title
-      status
-      description
-    }
-  }
-`);
-
-const DELETE_TASK = graphql(`
-  mutation DeleteTask($deleteTaskId: Int!) {
-    deleteTask(id: $deleteTaskId) {
-      title
-      status
-      id
-      description
-    }
-  }
-`);
+import Link from "next/link";
+import { graphql } from "../gql";
 
 const STATUS_OPTIONS = [
   {
@@ -51,37 +20,61 @@ const STATUS_OPTIONS = [
   },
 ];
 
+export const GET_ALL_TASKS = graphql(`
+  query GetAllTasks {
+    getAllTasks {
+      description
+      status
+      title
+      id
+      subTasks {
+        body
+        id
+      }
+    }
+  }
+`);
+
+export const CREATE_TASK = graphql(`
+  mutation CreateTask($title: String!, $status: String!, $description: String) {
+    createTask(title: $title, status: $status, description: $description) {
+      title
+      status
+      description
+    }
+  }
+`);
+
+export const DELETE_TASK = graphql(`
+  mutation DeleteTask($deleteTaskId: Int!) {
+    deleteTask(id: $deleteTaskId) {
+      title
+      status
+      id
+      description
+    }
+  }
+`);
 export default function Home() {
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState(STATUS_OPTIONS[0].value);
   const [description, setDescription] = useState("");
+
   const {
     data: taskData,
     loading: loadingTask,
     error: errorTask,
   } = useQuery(GET_ALL_TASKS);
 
-  const [
-    createTask,
-    {
-      data: createTaskData,
-      loading: loadingCreateTask,
-      error: errorCreateTask,
-    },
-  ] = useMutation(CREATE_TASK, {
-    refetchQueries: [GET_ALL_TASKS],
-  });
+  const [createTask, { loading: loadingCreateTask, error: errorCreateTask }] =
+    useMutation(CREATE_TASK, {
+      refetchQueries: [GET_ALL_TASKS],
+    });
 
-  const [
-    deleteTask,
-    {
-      data: deleteTaskData,
-      loading: loadingDeleteTask,
-      error: errorDeleteTask,
-    },
-  ] = useMutation(DELETE_TASK, {
-    refetchQueries: [GET_ALL_TASKS],
-  });
+  const [deleteTask, { loading: loadingDeleteTask, error: errorDeleteTask }] =
+    useMutation(DELETE_TASK, {
+      refetchQueries: [GET_ALL_TASKS],
+    });
 
   if (loadingTask || loadingCreateTask || loadingDeleteTask) {
     return <h2>Loading...</h2>;
@@ -97,17 +90,16 @@ export default function Home() {
     }
     return <p>Something unexpected has happened. Please reload the page.</p>;
   }
-  if (!taskData || createTaskData || deleteTaskData) {
+  if (!taskData) {
     return null;
   }
 
   const onDelete = (id: number) => {
-    console.log("delete", id);
     deleteTask({
       variables: { deleteTaskId: id },
     });
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const taskSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = {
       title,
@@ -134,7 +126,7 @@ export default function Home() {
         <h1 className={styles.title}>Welcome to the Task Manager</h1>
 
         <h2>Create a task</h2>
-        <form className={styles.card} onSubmit={onSubmit}>
+        <form className={styles.card} onSubmit={taskSubmit}>
           <div className={styles.inputGroup}>
             <label htmlFor="email" className={styles.label}>
               Title
@@ -188,10 +180,18 @@ export default function Home() {
         {taskData.getAllTasks.map((task) => {
           return (
             <div key={task.id} className={styles.card}>
-              <h2>Title: {task.title}</h2>
-              <p>{task.id}</p>
-              <p>Description: {task.description}</p>
-              <p>Status: {task.status}</p>
+              <Link href={`/task/${task.id}`}>
+                <h2>Title: {task.title}</h2>
+                <p>Description: {task.description}</p>
+                <p>Status: {task.status}</p>
+                {task.subTasks.map((subTask) => {
+                  return (
+                    <ul key={subTask.id} className={styles.list}>
+                      <li>{subTask.body}</li>
+                    </ul>
+                  );
+                })}
+              </Link>
               <button
                 className={styles.closeButton}
                 onClick={() => onDelete(task.id)}
--- src/pages/task/[taskId].tsx
+++ src/pages/task/[taskId].tsx
@@ -1,50 +1,113 @@
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
-import { useQuery } from "@apollo/client";
-import { graphql } from "../../gql";
+import { useMutation, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { graphql } from "../../gql";
 
-const GET_TASK = graphql(`
-  query GetTask($id: Int!) {
-    getTask(id: $id) {
+export const CREATE_SUBTASK = graphql(`
+  mutation CreateSubtask($taskId: Int!, $body: String!) {
+    createSubtask(taskId: $taskId, body: $body) {
+      body
       id
+    }
+  }
+`);
+
+export const GET_TASK = graphql(`
+  query GetTask($getTaskId: Int!) {
+    getTask(id: $getTaskId) {
+      status
       title
+      id
       description
+      subTasks {
+        body
+        id
+      }
     }
   }
 `);
 
 export default function Task() {
+  const [subTask, setSubTask] = useState("");
+
   const { query } = useRouter();
   const taskId =
     typeof query["taskId"] === "string" ? query["taskId"] : undefined;
 
-  const { data, loading, error } = useQuery(GET_TASK, {
-    variables: taskId ? { id: Number(taskId) } : undefined,
+  const {
+    data: taskData,
+    loading: taskLoading,
+    error: taskError,
+  } = useQuery(GET_TASK, {
+    variables: taskId ? { getTaskId: Number(taskId) } : undefined,
   });
-  if (loading) {
+  const [
+    createSubTask,
+    { data: subTaskData, loading: subTaskLoading, error: subTaskError },
+  ] = useMutation(CREATE_SUBTASK, {
+    refetchQueries: [GET_TASK],
+  });
+
+  if (taskLoading || subTaskLoading) {
     return <h2>Loading...</h2>;
   }
 
-  if (error) {
-    console.error(error);
+  if (taskError || subTaskError) {
+    if (taskError) {
+      console.error(taskError);
+    } else console.error(subTaskError);
     return null;
   }
-  if (!data) {
+  if (!taskData) {
     return null;
   }
+
+  const onClick = (id: number) => {
+    console.log(id);
+    createSubTask({
+      variables: {
+        taskId: Number(taskId),
+        body: subTask,
+      },
+    });
+  };
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Task {data.getTask.id}</title>
+        <title>Task {taskData.getTask.id}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Task {data.getTask.id}</h1>
-        <h2 className={styles.description}>{data.getTask.title}</h2>
-        <p className={styles.description}>{data.getTask.description}</p>
+        <h1 className={styles.title}>Task {taskData.getTask.id}</h1>
+        <h2 className={styles.description}>{taskData.getTask.title}</h2>
+        <p className={styles.description}>{taskData.getTask.description}</p>
+        <div className={styles.createSubtask}>
+          <input
+            type="subTask"
+            name="subTask"
+            id="subTask"
+            value={subTask}
+            onChange={(e) => setSubTask(e.currentTarget.value)}
+            className={styles.input}
+            placeholder="Sub Task Text"
+          />
+
+          <button onClick={() => onClick(taskData.getTask.id)}>
+            Add sub task
+          </button>
+        </div>
+        {taskData.getTask.subTasks.map((subTask) => {
+          return (
+            <ul className={styles.list} key={subTask.id}>
+              <li>{subTask.body}</li>
+              <button className={styles.smCloseButton}>x</button>
+            </ul>
+          );
+        })}
       </main>
     </div>
   );
